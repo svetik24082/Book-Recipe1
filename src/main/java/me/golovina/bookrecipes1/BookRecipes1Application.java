9696f648f79d680bb7928ddf3e1e283779ffd398
Света <svetik24082@mail.ru>
2023-01-04 22:42:26 +0500

    1  коммит

---
+++ src/main/java/me/golovina/bookrecipes1/BookRecipes1Application.java
@@ -0,0 +1,13 @@
+package me.golovina.bookrecipes1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecipes1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRecipes1Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/golovina/bookrecipes1/BookRecipes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.golovina.bookrecipes1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecipes1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
